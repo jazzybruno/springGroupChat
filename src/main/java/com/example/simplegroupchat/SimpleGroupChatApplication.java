e1dc2056517fbbd75c0db11f7d9be8ff3dfaa074
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-05-28 06:09:39 +0200

    Initialised the simple Group Chat Application

---
+++ src/main/java/com/example/simplegroupchat/SimpleGroupChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplegroupchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGroupChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleGroupChatApplication.class, args);
+    }
+
+}
